91f9b17d44e16ef0ea47e3cc31b9048634dc3abd
Dane Ericksen <10721110@my.uvu.edu>
2019-09-03 15:54:40 -0600

    Added Player, Bomb, & particle effect
    learned about events and classes

---
+++ New Unity Project/Assets/Scripts/Bomb.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Object = System.Object;
+
+public class Bomb : MonoBehaviour
+{
+    public UnityEvent Event;
+    private void OnCollisionEnter(Collision other)
+    {
+        Event.Invoke();
+    }
+
+    private void OnDestroy()
+    {
+        Event.Invoke();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- New Unity Project/Assets/Scripts/VarsClass.cs
+++ New Unity Project/Assets/Scripts/VarsClass.cs
@@ -1,9 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class VarsClass : MonoBehaviour
 {
+
+    public float floatValue;
+    public int intValue;
+    public string stringValue = "bob";
+    public int firePower;
+    public UnityEvent Event;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        Event.Invoke();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
