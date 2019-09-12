2712f45e1d24a8336bd751c05c302e96e3127661
Dane Ericksen <10721110@my.uvu.edu>
2019-09-12 15:52:24 -0600

    Dabbled with some particles and new events
    particle controller and on mouse down

---
+++ New Unity Project/Assets/Scripts/CatClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CatClass : MonoBehaviour
+{
+    public UnityEvent lightOff;
+    private void OnMouseDown()
+    {
+        lightOff.Invoke();
+    }
+}
---
+++ New Unity Project/Assets/Scripts/ParticleControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleControl : MonoBehaviour
+{
+    public ParticleSystem explosion;
+    // Start is called before the first frame update
+    public void OnMouseDown()
+    {
+        explosion.Emit(100);
+    }
+}
+
