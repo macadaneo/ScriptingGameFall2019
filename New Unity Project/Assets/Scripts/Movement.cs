55b205735b79dfc128bb17fe6752666610eeb460
Dane Ericksen <10721110@my.uvu.edu>
2019-09-17 16:00:01 -0600

    added character movement

---
+++ New Unity Project/Assets/Scripts/Movement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float gravity = 9.81f;
+    public float speed = 1f;
+    public float jumpSpeed = 75f;
+    private int jumpCount = 2;
+    
+    private Vector3 position;
+    public CharacterController controller;
+
+     void Update()
+     {
+         position.x = speed * Input.GetAxis("Horizontal");
+
+         if (!controller.isGrounded)
+         {
+             position.y -= gravity;
+         }
+         else
+         {
+             jumpCount = 2;
+         }
+
+         if (Input.GetButtonDown("Jump") && jumpCount > 0)
+         {
+             position.y = jumpSpeed;
+             jumpCount--;
+         }
+         
+         controller.Move(position*Time.deltaTime);
+     }
+}
