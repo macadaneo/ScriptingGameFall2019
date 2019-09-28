10a88b3d249d4c6afd1d91c8fad15b7f9f565fa8
Dane C. Ericksen <10721110@my.uvu.edu>
2019-09-28 00:03:22 -0600

    Edited Layer interaction and created Health collectible prefab

--- New Unity Project/Assets/Scripts/Collectible.cs
+++ New Unity Project/Assets/Scripts/Collectible.cs
@@ -8,11 +8,15 @@ public class Collectible : MonoBehaviour
     public UnityEvent Event;
     public GameObject collectibleEffect;
 
-    private void OnControllerColliderHit(ControllerColliderHit hit)
+    private void OnTriggerEnter(Collider other)
+    {
+            Destroy(gameObject);
+    }
+
+    private void OnDestroy()
     {
         Debug.Log("HealthUp");
-            Instantiate(collectibleEffect, transform.position, transform.rotation);
-            Event.Invoke();
-            Destroy(gameObject);
+        Instantiate(collectibleEffect, transform.position, transform.rotation);
+        Event.Invoke();
     }
 }
