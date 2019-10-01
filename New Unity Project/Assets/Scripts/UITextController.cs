32db09a2141bafd269d4183a030abfc60a77f4d1
Dane Ericksen <10721110@my.uvu.edu>
2019-10-01 16:00:41 -0600

    learned UI update

---
+++ New Unity Project/Assets/Scripts/UITextController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class UITextController : MonoBehaviour
+{
+    private Text textLabel;
+    public UnityEvent startEvent;
+    
+    public void UpdateText(IntData data)
+    {
+        textLabel.text = data.value.ToString();
+    }
+
+    void Start()
+    {
+        textLabel = GetComponent<Text>();
+        startEvent.Invoke();
+    }
+}
