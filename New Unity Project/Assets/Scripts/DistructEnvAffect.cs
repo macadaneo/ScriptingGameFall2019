8bb27056b20d8a8001c4e8d64d4c1878753c6403
Dane C. Ericksen <10721110@my.uvu.edu>
2019-10-09 23:57:33 -0600

    Created destructible wall

---
+++ New Unity Project/Assets/Scripts/DistructEnvAffect.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistructEnvAffect : DistructibleEnv
+{
+    public void start()
+        {
+            Distructable = false;
+        }
+        
+        public void OnTriggerEnter(Collider other)
+            {
+                Distructable = true;
+            }
+        
+        
+        
+            public void OnTriggerExit(Collider other)
+            {
+                Distructable = false;
+            }
+    
+            public void OnDestroy()
+            {
+                if (Distructable == true)
+                {
+                    Event.Invoke();
+                    Debug.Log("Environment Destroyed.");
+                }
+            }
+}
---
+++ New Unity Project/Assets/Scripts/DistructibleEnv.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+
+public class DistructibleEnv : MonoBehaviour
+{
+  public UnityEvent Event;
+  public bool Distructable = false;
+  private ParticleSystem rubble;
+  private void OnTriggerEnter(Collider other)
+  {
+    Distructable = true;
+  }
+
+  private void OnTriggerExit(Collider other)
+  {
+    Distructable = false;
+  }
+
+  private void OnDestroy()
+  {
+    Instantiate(rubble, transform.position, transform.rotation);
+    Destroy(rubble);
+  }
+}
