14583750a58fc5968d45a520639deb03f0f4026f
Dane C. Ericksen <10721110@my.uvu.edu>
2019-10-14 23:59:45 -0600

    Worked on throwing my grenade.

---
+++ New Unity Project/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public GameObject throwableItem;
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.E))
+            Instantiate(throwableItem, transform.position, transform.rotation);
+    }
+
+}
