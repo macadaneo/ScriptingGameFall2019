83a015c6e1b3bd59a0e1e91caa875c7901b4869e
Dane Ericksen <10721110@my.uvu.edu>
2019-11-12 15:52:05 -0700

    Leared Coroutines

---
+++ New Unity Project/Assets/Scripts/Coroutines.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Coroutines : MonoBehaviour
+{
+    public int counter = 10;
+    private WaitForSeconds wfsObj;
+    public float seconds = 1f;
+    public bool canRun;
+    public UnityEvent startEvent, repeatEvent, endEvent;
+    
+    
+    
+    private void Awake()
+    {
+        wfsObj = new WaitForSeconds(seconds);
+    }
+
+    public void CallCoroutine()
+    {
+        StartCoroutine(RunCoroutine());
+    }
+    
+    IEnumerator RunCoroutine()
+    {
+        startEvent.Invoke();
+        while (counter > 0)
+        {
+            repeatEvent.Invoke();
+            yield return wfsObj;
+            Debug.Log(counter);
+            counter--;
+        }
+
+        endEvent.Invoke();
+        yield return wfsObj;
+        Debug.Log("GO");
+        
+         while (canRun)
+         {
+             repeatEvent.Invoke();
+             yield return wfsObj;
+             Debug.Log(counter);
+             counter--;
+         }
+         endEvent.Invoke();
+         yield return wfsObj; 
+         Debug.Log("GO");
+    }
+}
