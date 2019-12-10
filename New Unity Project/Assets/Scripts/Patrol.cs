f8337d8b481f43e8b3a74f259f1439fc11e2c0a5
Dane C. Ericksen <10721110@my.uvu.edu>
2019-12-09 23:59:28 -0700

    Updated enemy to be completely random

--- New Unity Project/Assets/Scripts/Patrol.cs
+++ New Unity Project/Assets/Scripts/Patrol.cs
@@ -9,25 +9,27 @@ public class Patrol : MonoBehaviour
     public float speed;
     private float waitTime;
     public float startWaitTime;
+    public float minX, minY, maxX, maxY;
     
-    public Transform[] moveSpots;
+    public Transform moveSpot;
     private int randomSpot;
     
 
     void Start()
     {
-        randomSpot = Random.Range(0, moveSpots.Length);
+        waitTime = startWaitTime;
+        moveSpot.position = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY));
     }
 
     private void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, moveSpot.position, speed * Time.deltaTime);
 
-        if (Vector3.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
+        if (Vector3.Distance(transform.position, moveSpot.position) < 0.2f)
         {
             if (waitTime <= 0)
             {
-                randomSpot = Random.Range(0, moveSpots.Length);
+                moveSpot.position = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY));
                 waitTime = startWaitTime;
             }
             else
@@ -37,3 +39,4 @@ public class Patrol : MonoBehaviour
         }
     }
 }
+//made with help from Blackthronprod
