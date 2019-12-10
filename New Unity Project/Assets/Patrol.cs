d14dfefc1110c508f7df98f5657212218b8abb39
Dane C. Ericksen <10721110@my.uvu.edu>
2019-12-09 23:42:05 -0700

    Added moving enemy with Vector3 Array

---
+++ New Unity Project/Assets/Patrol.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Patrol : MonoBehaviour
+{
+    public float speed;
+    private float waitTime;
+    public float startWaitTime;
+    
+    public Transform[] moveSpots;
+    private int randomSpot;
+    
+
+    void Start()
+    {
+        randomSpot = Random.Range(0, moveSpots.Length);
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+                randomSpot = Random.Range(0, moveSpots.Length);
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+}
